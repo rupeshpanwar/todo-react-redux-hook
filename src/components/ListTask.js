3e45331ce2e0858df808cfb5d412223b3eaa6e1b
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-06-07 11:58:01 +0530

    List task Screen

--- src/components/ListTask.js
+++ src/components/ListTask.js
@@ -1,16 +1,35 @@
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { Col, Container, Row, Button } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
 import { getTask } from '../store/task/taskAction'
+
 
 export default function ListTask() {
     const dispatch = useDispatch()
+
+    const taskListSelector = useSelector((state) => state.task.taskList)
 
     useEffect(() => {
         dispatch(getTask())
     }, [])
     return (
-        <div>
-
-        </div>
+        <Container>
+            <Row className='justify-content-center mt-5'>
+                <Col>
+                    <ul className="list-group">
+                        {taskListSelector.map(task =>
+                        (
+                            <li className="list-group-item d-flex justify-content-between" key={task.id}>
+                                {task.title}
+                                <Button size="sm" variant="outline-danger">
+                                    <i className="fas fa-trash"></i>
+                                </Button>
+                            </li>
+                        )
+                        )}
+                    </ul>
+                </Col>
+            </Row>
+        </Container>
     )
 }
